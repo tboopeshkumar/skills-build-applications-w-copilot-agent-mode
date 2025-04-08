1ecb660d7f769ffc06957d3e5108e0b80c7885d3
Boopesh Kumar <tboopeshkumar@gmail.com>
2025-04-08 17:20:46 +0000

    new changes

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,47 +1,73 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
-from octofit_tracker.test_data import test_data
+from octofit_tracker.test_data import test_users, test_teams, test_activities, test_leaderboard, test_workouts
+from django.db import connection
 from datetime import timedelta
 
 class Command(BaseCommand):
     help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
-        # Clear existing data
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
+        # Clear existing data using raw SQL for efficiency
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM octofit_tracker_user;")
+            cursor.execute("DELETE FROM octofit_tracker_team_members;")
+            cursor.execute("DELETE FROM octofit_tracker_team;")
+            cursor.execute("DELETE FROM octofit_tracker_activity;")
+            cursor.execute("DELETE FROM octofit_tracker_leaderboard;")
+            cursor.execute("DELETE FROM octofit_tracker_workout;")
 
         # Populate users
-        users = []
-        for user_data in test_data['users']:
-            user = User(username=user_data['username'], email=user_data['email'], password=user_data['password'])
-            user.save()
-            users.append(user)
+        users = {}
+        for user_data in test_users:
+            user = User.objects.create(
+                email=user_data['email'],
+                name=user_data.get('name', user_data['username']),
+                age=user_data.get('age', 18),
+                username=user_data['username'],
+                password=user_data['password']
+            )
+            users[user.username] = user
 
         # Populate teams
-        for team_data in test_data['teams']:
-            team = Team(name=team_data['name'])
-            team.save()
+        for team_data in test_teams:
+            team = Team.objects.create(name=team_data['name'])
+            team.members.add(*[users[username] for username in team_data['members']])
 
         # Populate activities
-        for activity_data in test_data['activities']:
-            user = User.objects.get(username=activity_data['username'])
-            duration_minutes = activity_data['duration'].total_seconds() // 60
-            activity = Activity(user=user, activity_type=activity_data['activity_type'], duration=duration_minutes)
-            activity.save()
+        activity_objects = [
+            Activity(
+                user=users[activity_data['user']],
+                activity_type=activity_data['activity_type'],
+                duration=int(timedelta(
+                    hours=int(activity_data['duration'].split(':')[0]),
+                    minutes=int(activity_data['duration'].split(':')[1])
+                ).total_seconds() // 60),
+                date=activity_data.get('date', '2025-04-08')
+            )
+            for activity_data in test_activities
+        ]
+        Activity.objects.bulk_create(activity_objects)
 
         # Populate leaderboard
-        for leaderboard_data in test_data['leaderboard']:
-            user = User.objects.get(username=leaderboard_data['username'])
-            leaderboard = Leaderboard(user=user, score=leaderboard_data['score'])
-            leaderboard.save()
+        leaderboard_objects = [
+            Leaderboard(
+                user=users[leaderboard_data['user']],
+                score=leaderboard_data['score']
+            )
+            for leaderboard_data in test_leaderboard
+        ]
+        Leaderboard.objects.bulk_create(leaderboard_objects)
 
         # Populate workouts
-        for workout_data in test_data['workouts']:
-            workout = Workout(name=workout_data['name'], description=workout_data['description'])
-            workout.save()
+        workout_objects = [
+            Workout(
+                name=workout_data['name'],
+                description=workout_data['description'],
+                duration=workout_data.get('duration', 60)
+            )
+            for workout_data in test_workouts
+        ]
+        Workout.objects.bulk_create(workout_objects)
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
--- octofit-tracker/backend/octofit_tracker/test_data.py
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -1,34 +1,38 @@
-test_data = {
-    "users": [
-        {"username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
-        {"username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
-        {"username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
-        {"username": "crashoverride", "email": "crashoverride@hmhigh.edu", "password": "crashoverridepassword"},
-        {"username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
-    ],
-    "teams": [
-        {"name": "Blue Team"},
-        {"name": "Gold Team"},
-    ],
-    "activities": [
-        {"username": "thundergod", "activity_type": "Cycling", "duration": "1:00:00"},
-        {"username": "metalgeek", "activity_type": "Crossfit", "duration": "2:00:00"},
-        {"username": "zerocool", "activity_type": "Running", "duration": "1:30:00"},
-        {"username": "crashoverride", "activity_type": "Strength", "duration": "0:30:00"},
-        {"username": "sleeptoken", "activity_type": "Swimming", "duration": "1:15:00"},
-    ],
-    "leaderboard": [
-        {"username": "thundergod", "score": 100},
-        {"username": "metalgeek", "score": 90},
-        {"username": "zerocool", "score": 95},
-        {"username": "crashoverride", "score": 85},
-        {"username": "sleeptoken", "score": 80},
-    ],
-    "workouts": [
-        {"name": "Cycling Training", "description": "Training for a road cycling event"},
-        {"name": "Crossfit", "description": "Training for a crossfit competition"},
-        {"name": "Running Training", "description": "Training for a marathon"},
-        {"name": "Strength Training", "description": "Training for strength"},
-        {"name": "Swimming Training", "description": "Training for a swimming competition"},
-    ],
-}
+# Test data for populating the octofit_db database
+
+test_users = [
+    {"username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+    {"username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+    {"username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+    {"username": "crashoverride", "email": "crashoverride@mhigh.edu", "password": "crashoverridepassword"},
+    {"username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+]
+
+test_teams = [
+    {"name": "Blue Team", "members": ["thundergod", "metalgeek"]},
+    {"name": "Gold Team", "members": ["zerocool", "crashoverride", "sleeptoken"]},
+]
+
+test_activities = [
+    {"user": "thundergod", "activity_type": "Cycling", "duration": "1:00:00"},
+    {"user": "metalgeek", "activity_type": "Crossfit", "duration": "2:00:00"},
+    {"user": "zerocool", "activity_type": "Running", "duration": "1:30:00"},
+    {"user": "crashoverride", "activity_type": "Strength", "duration": "0:30:00"},
+    {"user": "sleeptoken", "activity_type": "Swimming", "duration": "1:15:00"},
+]
+
+test_leaderboard = [
+    {"user": "thundergod", "score": 100},
+    {"user": "metalgeek", "score": 90},
+    {"user": "zerocool", "score": 95},
+    {"user": "crashoverride", "score": 85},
+    {"user": "sleeptoken", "score": 80},
+]
+
+test_workouts = [
+    {"name": "Cycling Training", "description": "Training for a road cycling event"},
+    {"name": "Crossfit", "description": "Training for a crossfit competition"},
+    {"name": "Running Training", "description": "Training for a marathon"},
+    {"name": "Strength Training", "description": "Training for strength"},
+    {"name": "Swimming Training", "description": "Training for a swimming competition"},
+]
